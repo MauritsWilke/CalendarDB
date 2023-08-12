6ea2f79aa807ac17edf6a10ffcfcfc42c0d87769
Maurits Wilke <mauritswilke@gmail.com>
2023-08-12 11:48:02 +0200

    feat: :fire: fresh start, added a head component

--- src/lib/scripts/stores.ts
+++ src/lib/scripts/stores.ts
@@ -2,18 +2,3 @@ import { onAuthStateChanged, type User } from "firebase/auth";
 import { writable } from "svelte/store";
 import { auth } from "./firebase";
 
-function userStore() {
-	let unsubscribe: () => void;
-
-	const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
-		unsubscribe = onAuthStateChanged(auth, (user) => {
-			set(user);
-		})
-
-		return () => unsubscribe();
-	})
-
-	return { subscribe };
-}
-
-export const currentUser = userStore();
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-hi
+<script lang="ts">
+    import Head from "$lib/components/Head.svelte";
+</script>
+
+<Head title={"App | CalendarDB"} description={"CalendarDB homepage"} />
+
+<p>This is the app page</p>
