5fab6df8c8fa1f596e93b6de86feb3003a5d2d95
Maurits Wilke <mauritswilke@gmail.com>
2024-01-02 22:12:42 +0100

    feat: :tada: initial commit

--- src/lib/scripts/firebase/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./auth/signInWithGoogle";
-export { _signInWithEmailAndPassword as signInWithEmailAndPassword } from "./auth/signInWithEmailPassword";
-export { _signOut as signOut } from "./auth/signOut";
--- src/lib/scripts/utils/util.ts
+++
@@ -1,8 +0,0 @@
-export function debounce<T extends (...args: Parameters<T>) => void>(this: ThisParameterType<T>, fn: T, ms = 300) {
-	let timeoutId: ReturnType<typeof setTimeout>;
-
-	return (...args: Parameters<T>) => {
-		clearTimeout(timeoutId);
-		timeoutId = setTimeout(() => fn.apply(this, args), ms);
-	};
-};
